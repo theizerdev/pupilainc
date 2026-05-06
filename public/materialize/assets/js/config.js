e6d0e42af44bc2b0f043f8f689d0ff64fb5c4dd3
Your Name <you@example.com>
2026-05-06 04:13:13 +0000

    06052026 cambios en seeder de paises y en config.js

--- public/materialize/assets/js/config.js
+++ public/materialize/assets/js/config.js
@@ -73,7 +73,7 @@ if (typeof TemplateCustomizer !== 'undefined') {
   window.templateCustomizer = new TemplateCustomizer({
     displayCustomizer: false,
     lang: localStorage.getItem('templateCustomizer-' + templateName + '--Lang') || 'en', // Set default language here
-    // defaultPrimaryColor: '#D11BB4',
+    defaultPrimaryColor: '#1a9fb7',
     // defaultSkin: 1,
     // defaultTheme: 'system',
     // defaultSemiDark: true,
