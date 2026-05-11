6df872dbb6420b210adea51c5e16dc584bbf9fe8
theizerdev <theizerdev@gmail.com>
2026-05-11 17:06:33 -0400

    11052026 Creacion de sucursales Issues 07052026

--- resources/views/components/layouts/horizontal.blade.php
+++ resources/views/components/layouts/horizontal.blade.php
@@ -12,7 +12,7 @@
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <title>{{ 'HOSPITAL ANGELES' }}</title>
+    <title>{{ 'PUPILA INC' }}</title>
     <meta name="description" content="{{ config('app.name') }} - {{ config('app.description') }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
